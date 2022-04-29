fdeea7b27faa200975e58b6149f6f162c08f2f5f
Larry Li <86larryli@gmail.com>
2022-04-29 01:41:44 -0400

    Updated global URL

--- src/GlobalVars.vue
+++ src/GlobalVars.vue
@@ -1,6 +1,6 @@
 <script>
-// const requestURL = "http://api.takemehome.flights:3000";
-const requestURL = "http://86larrylilocaldev.com/api";
+const requestURL = "http://api.takemehome.flights:3000";
+// const requestURL = "http://86larrylilocaldev.com/api";
 
 export default {
   requestURL,
